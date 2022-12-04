09d9dd59ce4c9a6c28b44231ec421a6598a06277
Jean Saavedra <86541786+JeanS44@users.noreply.github.com>
2022-12-04 00:32:01 -0300

    Update mochila.py

--- mochila.py
+++ mochila.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 """ import igraph as ig """
 import math
+import random
 
 def definirVectorProbabilidades(rand_solution, tau):
     vector_probabilidades = np.array([], dtype=np.double)
@@ -25,25 +26,48 @@ def definirVectorRuleta(vector_proporcion):
         vector_ruleta = np.append(vector_ruleta, vector_proporcion[i]+vector_ruleta[i-1])
     return vector_ruleta
 
-def determinarFitness(vector_beneficio):
-    sum_v_beneficio = np.sum(vector_beneficio)
+def determinarFitness(vector_beneficio, vector_peso, vector_solucion, cap_max):
+    suma_total = 0
     vector_fitness = np.array([])
-    for i in range(len(vector_beneficio)):
-        vector_fitness = np.append(vector_fitness, np.ceil(np.divide((vector_beneficio[i]*100),sum_v_beneficio)))
-    """ return "La suma total fue: ", suma_total, "el fitness es: ", fitness, " y la capacidad máxima de la mochila es: ", maximo_mochila """
-    """ return fitness """
-    return vector_fitness
+    for i in range(len(vector_solucion)):
+        if vector_solucion[i] == 1:
+            suma_total += vector_peso[i]
+    if suma_total <= cap_max:
+        sum_v_beneficio = np.sum(vector_beneficio)
+        for i in range(len(vector_beneficio)):
+            vector_fitness = np.append(vector_fitness, np.divide((vector_beneficio[i]*100),sum_v_beneficio))
+        """ return "La suma total fue: ", suma_total, "el fitness es: ", fitness, " y la capacidad máxima de la mochila es: ", maximo_mochila """
+        """ return fitness """
+        return vector_fitness
 
-def determinarPosArrayOrdenado(vector_fitness, vector_fitness_sorted):
-    array = []
+def seleccionarComponenteJ(vector_fitness_ordenado, vector_fitness):
+    array = np.array([])
+    for i in range(len(vector_fitness)):
+        for j in range(len(vector_fitness_ordenado)):
+            if vector_fitness_ordenado[i] == vector_fitness[j]:
+                pos = j
+                array = np.append(array, pos)
     return array
 
-if len(sys.argv) == 3:
+def determinarSelección(vector_solucion, vector_ruleta):
+    vector_seleccion = np.zeros(len(vector_solucion))
+    while len(vector_seleccion) != 10:
+        for i in range(0, len(vector_ruleta)):
+            azar = random.uniform(0, 1)
+            print("Azar", azar)
+            if azar <= vector_ruleta[i]:
+                pos = i
+                print("Pos", azar, pos, vector_ruleta[i])
+                vector_seleccion = np.append(vector_seleccion, vector_solucion[pos])
+        return vector_seleccion
+
+if len(sys.argv) == 4:
     # Asignación de parámetros.
     np.set_printoptions(threshold=sys.maxsize)
     np.set_printoptions(suppress=True)
     seed = int(sys.argv[1])
-    tau = float(sys.argv[2])
+    iteraciones = int(sys.argv[2])
+    tau = float(sys.argv[3])
     print("Nombre del archivo: ", sys.argv[0], " Seed:", seed, " Tau:", tau)
     # Término de asignación de parámetros.
     """ peso = np.array([94,506,416,992,649,237,457,815,446,422,791,359,667,598,7,544,334,766,994,893,633,131,428,700,617,874,720,419,794,196,997,116,908,539,707,569,537,931,726,487,772,513,81,943,58,303,764,536,724,789]) """
@@ -58,15 +82,19 @@ if len(sys.argv) == 3:
     print("Vector de proporciones:\n",v_proporciones)
     v_ruleta = definirVectorRuleta(v_proporciones)
     print("Vector de ruleta:\n",v_ruleta)
-    v_fitness = determinarFitness(beneficio)
+    v_fitness = determinarFitness(beneficio, peso, random_solution, cap_max)
     print("Vector de fitness:\n", v_fitness)
+    
     v_fitness_sorted = np.array([], dtype=int)
     v_fitness_sorted = np.sort(v_fitness, kind='mergesort')
     print("Vector de fitness ordenado:\n", v_fitness_sorted)
-    pos_arr_ordenado = determinarPosArrayOrdenado(v_fitness, v_fitness_sorted)
-    print(pos_arr_ordenado)
+    componente_j = seleccionarComponenteJ(v_fitness_sorted, v_fitness)
+    print("Vector de componente J:\n",componente_j)
+    seleccion = determinarSelección(random_solution,v_ruleta)
+    print(seleccion)
+        
 else:
     print("Porfavor reingrese los parámetros de manera correcta.")
     print("Parametros a ingresar: 'Nombre del archivo' 'Semilla' 'Tau'")
-    # .\mochila.py 1 1.4
+    # .\mochila.py 1 50 1.4
     sys.exit(0)
