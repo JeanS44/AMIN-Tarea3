6188501fc1084574771bcba800464b145a763118
Jean Saavedra <86541786+JeanS44@users.noreply.github.com>
2022-12-22 23:12:09 -0300

    Subida

--- mochila.py
+++ mochila.py
@@ -26,19 +26,18 @@ def definirVectorRuleta(vector_proporcion):
         vector_ruleta = np.append(vector_ruleta, vector_proporcion[i]+vector_ruleta[i-1])
     return vector_ruleta
 
-def determinarFitness(vector_beneficio, vector_peso, vector_solucion, cap_max):
-    suma_total = 0
-    vector_fitness = np.array([])
+def determinarFitness(vector_beneficio, vector_peso, vector_solucion):
+    fitness = np.array([])
+    pi = np.array([])
+    max_pi = np.max(vector_beneficio)
+    for i in range(len(vector_beneficio)):
+        pi = np.append(pi, vector_beneficio[i]/(max_pi+1))
     for i in range(len(vector_solucion)):
-        if vector_solucion[i] == 1:
-            suma_total += vector_peso[i]
-    for i in range(len(vector_solucion)):
-        if suma_total <= cap_max and vector_solucion[i] == 1:
-            sum_v_beneficio = np.sum(vector_beneficio)
-            vector_fitness = np.append(vector_fitness, np.divide((vector_beneficio[i]*100),sum_v_beneficio))
-        else:
-            vector_fitness = np.append(vector_fitness, 0)
-    return vector_fitness
+        if vector_solucion[i] == 0:
+            fitness = np.append(fitness, vector_peso[i]+pi[i])
+        elif vector_solucion[i] == 1:
+            fitness = np.append(fitness, vector_beneficio[i]+(1-pi[i]))
+    return fitness
 
 def seleccionarComponenteJ(vector_fitness_ordenado, vector_fitness):
     array = np.array([])
@@ -62,20 +61,26 @@ def determinarSelección(vector_solucion, vector_ruleta, iteraciones):
                     vector_seleccion[pos] = 1
     return vector_seleccion
 
-if len(sys.argv) == 4:
+def rectificar(solucion_generada, solucion_seleccion):
+    
+
+if len(sys.argv) == 5:
     # Asignación de parámetros.
     np.set_printoptions(threshold=sys.maxsize)
     np.set_printoptions(suppress=True)
     seed = int(sys.argv[1])
     iteraciones = int(sys.argv[2])
     tau = float(sys.argv[3])
-    print("Nombre del archivo: ", sys.argv[0], " Seed:", seed, " Tau:", tau)
+    entrada = sys.argv[4]
+    print("Nombre del archivo: ", sys.argv[0], " Seed:", seed, " Tau:", tau, " Archivo:", entrada)
+    parametros = pd.read_csv(entrada, skiprows=1, delim_whitespace=True, header=None, nrows=3, usecols=[1]).to_numpy()
+    print(parametros)
+    n = int(parametros[0])     
+    c = int(parametros[1])     
+    datos = pd.read_csv(entrada, skiprows=5, delimiter=',', header=None, nrows=n, usecols=[1, 2, 3]).transpose().to_numpy()
+    print(datos)
     # Término de asignación de parámetros.
-    """ peso = np.array([94,506,416,992,649,237,457,815,446,422,791,359,667,598,7,544,334,766,994,893,633,131,428,700,617,874,720,419,794,196,997,116,908,539,707,569,537,931,726,487,772,513,81,943,58,303,764,536,724,789]) """
-    peso = np.array([95, 4 , 60, 32, 23, 72, 80, 62, 65, 46])
-    beneficio = np.array([55, 10, 47, 5, 4, 50, 8, 61, 85, 87])
-    cap_max = 269
-    random_solution = np.random.randint(2, size = len(peso))
+    random_solution = np.random.randint(2, size = len(datos[0]))
     print("Solución generada:\n", random_solution)
     v_probabilidades = definirVectorProbabilidades(random_solution, tau)
     """ print("Vector de probabilidades:\n",v_probabilidades) """
@@ -87,23 +92,18 @@ if len(sys.argv) == 4:
     i = 0
     while i < iteraciones:
         print("")
-        v_fitness = determinarFitness(beneficio, peso, random_solution, cap_max)
-        print("Vector de fitness:\n", v_fitness," y su suma es: ", np.sum(v_fitness))
+        v_fitness = determinarFitness(datos[0], datos[1], random_solution)
+        """ print("Vector de fitness:\n", v_fitness," y su suma es: ", np.sum(v_fitness)) """
         v_fitness_sorted = np.array([], dtype=int)
         v_fitness_sorted = np.sort(v_fitness, kind='mergesort')
         """ print("Vector de fitness ordenado:\n", v_fitness_sorted) """
         componente_j = seleccionarComponenteJ(v_fitness_sorted, v_fitness)
-        print("Vector de componente J:\n",componente_j)
+        """ print("Vector de componente J:\n",componente_j) """
         seleccion = determinarSelección(random_solution,v_ruleta, 1000)
         print("Solución generada en selección:\n", seleccion)
-        v_fitness_seleccion = determinarFitness(beneficio, peso, seleccion, cap_max)
-        print("Vector de fitness:\n", v_fitness_seleccion," y su suma es: ", np.sum(v_fitness_seleccion))
-        if np.sum(v_fitness_seleccion) > np.sum(v_fitness):
-            v_fitness = v_fitness_seleccion
-        print(v_fitness)
         i += 1
 else:
     print("Porfavor reingrese los parámetros de manera correcta.")
-    print("Parametros a ingresar: 'Nombre del archivo' 'Semilla' 'Tau'")
-    # .\mochila.py 1 50 1.4
+    print("Parametros a ingresar: 'Nombre del archivo' 'Semilla' 'Tau' 'Nombre del archivo'")
+    # python .\mochila.py 1 50 1.4 knapPI_15_20_1000.csv
     sys.exit(0)
